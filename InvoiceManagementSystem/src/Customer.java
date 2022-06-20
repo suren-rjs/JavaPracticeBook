73078ba9ec8bc91caaf089920d1dbd090621dc0f
suren-rjs <suren.rjs@gmail.com>
2022-06-20 07:40:48 +0530

    Invoice management application prototype added

---
+++ InvoiceManagementSystem/src/Customer.java
@@ -0,0 +1,4 @@
+public class Customer {
+//    To Create and Update Customer
+    private static int customerId = 1;
+}
---
+++ InvoiceManagementSystem/src/Invoice.java
@@ -0,0 +1,4 @@
+public class Invoice {
+//    To Create and Update Invoice
+    private static int invoiceId = 1;
+}
